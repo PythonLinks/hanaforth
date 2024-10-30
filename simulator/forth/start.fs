e7578a56600148100abe56313812f50daa086eea
PythonLinks <34622952+PythonLinks@users.noreply.github.com>
2024-10-30 13:06:18 +0100

    This runs in simulation on Mac OS with the boot from 0 version of the following commit.
    commit 3f9c1ced172376e00f9dade4f731c4e54924360d (HEAD -> backtrack, master)
    Author: PythonLinks <34622952+PythonLinks@users.noreply.github.com>
    Date:   Wed Oct 30 12:07:34 2024 +0100
    
        Not working in simulation, and too large in synthesis.
        Backtracking to figure out the simulation problem.

---
+++ simulator/forth/start.fs
@@ -0,0 +1,26 @@
+: start
+spiStart
+nop
+spi! nop nop nop
+nop  nop nop nop
+nop  nop nop nop
+nop  nop nop nop
+
+spi! nop nop nop
+nop  nop nop nop
+nop  nop nop nop
+nop  nop nop 
+
+spi@ drop nop nop
+nop nop nop nop
+nop nop nop nop
+nop nop nop nop
+nop
+;    
+
+
+
+
+
+
+
